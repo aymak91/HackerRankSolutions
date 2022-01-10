df80ce2d2c7136d8e14f21c0bd344fb9ce32b57c
aymak91 <amakyu@gmail.com>
2022-01-10 14:23:40 -0800

    update 1-3-2022

--- 2022-01-03/MergeIntervals.js
+++ 2022-01-03/MergeIntervals.js
@@ -1,3 +1,25 @@
+// Given a collection of time intervals, [start, end], merge and return the overlapping 
+// intervals sorted in ascending order of their start times.
+
+// Example
+// intervals = [[7, 7], [2, 3], [6, 11], [1, 2]]
+
+// The interval [1, 2] merges with [2, 3] while [7, 7] merges with [6, 11]. There are no more overlapping intervals. The answer is [[1, 3], [6, 11]].
+
+// Function Description
+// Complete the function getMergedIntervals in the editor below.
+
+// getMergedIntervals has the following parameter(s):
+//     int intervals[[n][2]]:  the time intervals
+
+// Returns
+//     int[][2]: the merged intervals in sorted order
+
+// Constraints
+// 1 ≤ n ≤ 105
+// 1 ≤ intervals[i][2] ≤ 109
+// intervals[i][0] ≤ intervals[i][1] for all i.
+
 function getMergedIntervals(intervals) {
     // Write your code here
 
@@ -8,11 +30,12 @@ function getMergedIntervals(intervals) {
     
     for (let i=1; i<intervals.length; i++) {
         const lastIntervalIdx = outputArr.length-1;
-        const lastRightBound = outputArr[lastIntervalIdx][1]
-        const leftBound = intervals[i][0];
-        const rightBound = intervals[i][1];
+        const lastRightBound = outputArr[lastIntervalIdx][1] // 11
+        const leftBound = intervals[i][0]; //7
+        const rightBound = intervals[i][1]; // 7
 
-
+        // [[1,2], [2,3], [6,11], [7,7]]
+        // output = [[1,3], [6,11], [12, 18]]
         if (leftBound <= lastRightBound && rightBound > lastRightBound) outputArr[lastIntervalIdx][1] = rightBound;
         if (leftBound <= lastRightBound && rightBound <= lastRightBound) continue;
         if (leftBound > lastRightBound) outputArr.push(intervals[i]);
