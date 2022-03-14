d7f285120de647a06f219c9d58d096456aa20078
aymak91 <amakyu@gmail.com>
2022-03-14 14:35:33 -0700

    metro land festival

--- 2022-01-03/MergeIntervals.js
+++ 2022-01-03/MergeIntervals.js
@@ -16,8 +16,8 @@
 //     int[][2]: the merged intervals in sorted order
 
 // Constraints
-// 1 ≤ n ≤ 105
-// 1 ≤ intervals[i][2] ≤ 109
+// 1 ≤ n ≤ 10^5
+// 1 ≤ intervals[i][2] ≤ 10^9
 // intervals[i][0] ≤ intervals[i][1] for all i.
 
 function getMergedIntervals(intervals) {
@@ -27,6 +27,10 @@ function getMergedIntervals(intervals) {
     
     intervals.sort((a,b) => a[0] -b[0]);
     let outputArr = [intervals[0]];
+    let lastInterval = {
+        start: intervals[0][0],
+        end: intervals[0][1]
+    }
     
     for (let i=1; i<intervals.length; i++) {
         const lastIntervalIdx = outputArr.length-1;
@@ -40,4 +44,35 @@ function getMergedIntervals(intervals) {
        
     }
     return outputArr;
+}
+
+let getMergedIntervals = function(intervals) {
+	let res = [];
+	if (intervals.length === 1) return intervals;
+
+	// Iterate over sorted Intervals array
+	intervals.sort((a, b) => a[0] - b[0]);
+	
+    // Have curr interval start at intervals[0] 
+    let currInterval = {
+        start : intervals[0][0],
+        end : intervals[0][1],
+    }
+
+    for (let i = 1; i < intervals.length; i++){
+        let currStart = intervals[i][0];
+        let currEnd = intervals[i][1];
+    // Compare the end time of currInterval to intervals[i]’s start time to determine if      //mergeable		
+
+        if (currStart > currInterval.end) {
+            res.push([currInterval.start, currInterval.end]);
+            currInterval.start = currStart;
+        }
+
+        if (currEnd > currInterval.end) currInterval.end =  currEnd;
+    }
+
+    res.push([currInterval.start, currInterval.end]);
+
+    return res;
 }
