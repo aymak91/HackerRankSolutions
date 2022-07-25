5a56fb9946541e35911fa9708309a88969b1919a
aymak91 <amakyu@gmail.com>
2022-07-25 12:50:55 -0700

    best sum any tree path

---
+++ 2022-07-25/BestSumAnyTreePath..js
@@ -0,0 +1,22 @@
+// Given a tree rooted at node 0 and a value assigned to each node, determine the maximum sum of the values along any path in the tree. 
+// The path must not be empty, and in some cases it might not go through the root. The following tree (labeled node number/value) is analyzed below.
+
+// Two of the possible paths are 4 → 0 → 1 → 2 → 3 which has a sum of 15 + 5 + 7 + -10 + 4 = 21 and 1 → 2 → 3 with a sum of 7 + -10 + 4 = 1.  
+// A third possible path, the maximum sum path, is 4 → 0 → 1 with a sum of 15 + 5 + 7 = 27.
+
+ 
+
+// Function Description 
+// Complete the function bestSumAnyTreePath in the editor below. It must return an integer that represents the largest value sum on any non-empty path in the tree.
+
+// bestSumAnyTreePath has the following parameter(s):
+// parent int[]: integer array where parent[i] = j means that node j is a parent of node i. parent[0] will be set to -1 to indicate that node 0 is the root of the tree.
+// values int[]: integer array where values[i] denotes the value of node i
+   
+
+// Constraints
+// 1 ≤ n ≤ 10^5
+// parent[0] = -1 
+// 0 ≤ parent[i] < n for 1 ≤ i < n
+// -1000 ≤ values[i] ≤ 1000 
+// the parent array defines a valid tree
