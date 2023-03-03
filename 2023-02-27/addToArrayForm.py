21373fd2449f2d6da257cfc86bd0c3ec1805a52a
Josiah_L <sjosiahleon@gmail.com>
2023-03-03 14:57:51 -0800

    fix

--- 2023-02-27/addToArrayForm.py
+++ 2023-02-27/addToArrayForm.py
@@ -1,7 +1,7 @@
 def addToArrayForm(num, k) -> list:
     k_len = len(str(k))
     
-    if sum(num) < k_len:
+    if len(num) < k_len:
         num = ([0] * ((k_len) - len(num))) + num
 
     for i in range(len(num)-1, -1, -1):
