787a99084c216b46a7ea7368652fb4072066ee55
Josiah_L <sjosiahleon@gmail.com>
2023-03-06 14:36:50 -0800

    drying it out

--- 2023-03-06/remove_dups.py
+++ 2023-03-06/remove_dups.py
@@ -2,11 +2,7 @@ def removeDuplicates(s):
         stack = []
 
         for char in s:
-            if not stack:
-                stack.append(char)
-                continue
-
-            if char == stack[-1]:
+            if stack and char == stack[-1]:
                 stack.pop()
             else:
                 stack.append(char)
