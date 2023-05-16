fe40764b0fe69ae957b7e1088f738b0222ffb24d
Josiah_L <sjosiahleon@gmail.com>
2023-05-16 11:40:23 -0700

    added comments

--- 2023-05-15/container.py
+++ 2023-05-15/container.py
@@ -1,12 +1,32 @@
 def maxArea(height):
+    # Initialize the maximum water variable to negative infinity, this ensures every other value is larger than it
     max_water = float('-inf')
+    
+    # Initialize the left and right pointers to the first and last indices of the height list
     left, right = 0, len(height)-1
-    for i in range(len(height)-1, 0, -1):
+    
+    # Iterate in reverse order effectively giving us the "width" between the two pointers
+    for width in range(len(height)-1, 0, -1):
+        
+        # Determine the shortest wall
         if height[left] < height[right]:
+            
+            # The height of the wall at the left pointer becomes the shortest wall
             shortest_wall = height[left]
+            
+            # Increment the left pointer by 1
             left += 1
         else:
+            # The height of the wall at the right pointer becomes the shortest wall
             shortest_wall = height[right]
+            # Decrement the right pointer by 1
             right -= 1
-        max_water = max(max_water, shortest_wall * i)
-    return max_water
+        
+        # Calculate the current water area by multiplying the shortest wall height with the distance between the two pointers
+        current_area = shortest_wall * width
+        
+        # Update the max_water variable by taking the maximum value between the current max_water and the calculated water area
+        max_water = max(max_water, current_area)
+    
+    # Return the maximum amount of water that can be contained
+    return max_water  
--- 2023-05-15/ranks.py
+++ 2023-05-15/ranks.py
@@ -1,22 +1,33 @@
 from heapq import heappush, heappop
+
 def findRelativeRanks(score):
+    # Create an empty heap
     heap = list()
+
+    # Define ranks for medals
     ranks = {
         1: 'Gold Medal',
         2: 'Silver Medal',
         3: 'Bronze Medal'
     }
+
+    # Initialize the current rank
     rank = 1
 
+    # Push scores onto the heap, storing them as tuples (-num, idx)
     for idx, num in enumerate(score):
         heappush(heap, (-num, idx))
 
+    # Assign ranks to the scores
     for _ in range(len(score)):
+        # Pop the score with the highest value from the heap
         num, idx = heappop(heap)
-        if rank in ranks:
-            score[idx] = ranks[rank]
-        else:
-            score[idx] = str(rank)
+
+        # Check if the rank corresponds to a medal
+        score[idx] = ranks.get(rank, str(rank))
+
+        # Increment the rank for the next iteration
         rank += 1
 
+    # Return the modified score list
     return score
