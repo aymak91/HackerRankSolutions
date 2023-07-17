d3b30c1a78350fe1e1f0cc276ed426d3b974aea1
Josiah Leon <sjosiahleon@gmail.com>
2023-07-17 14:22:17 -0700

    added comments

--- 2023-07-17/delete_even.py
+++ 2023-07-17/delete_even.py
@@ -1,14 +1,19 @@
 def deleteEven(listHead):
+    # Create a dummy node and set it as the next node of the dummy
     dummy = SinglyLinkedListNode('dummy')
     dummy.next = listHead
     curr = dummy
     
+    # Iterate until there is a next node
     while curr.next:
+        # Check if the data of the next node is even
         if curr.next.data % 2 == 0:
+            # If the data is even, skip the next node by updating the next pointer
             curr.next = curr.next.next
             continue
             
+        # If the data is not even, move to the next node
         curr = curr.next
     
+    # Return the next node after the dummy node, which represents the modified linked list
     return dummy.next
-    
--- 2023-07-17/swap_pairs.py
+++ 2023-07-17/swap_pairs.py
@@ -1,22 +1,31 @@
 def swapPairs(head):
+    # If the list is empty or contains only one node, return the head as it is
     if not head or not head.next:
-            return head
-        
+        return head
+    
+    # Create a dummy node and set it as the next node of the dummy
     dummy = SinglyLinkedListNode('dummy')
     dummy.next = head
     curr = dummy
+    
+    # Iterate until there are at least two more nodes after the current node
     while curr.next and curr.next.next:
-
+        # Assign variables to the current node and the next node after it
         n_node = curr.next
         a_node = curr.next.next
-
-        nxt = a_node.next
-        a_node.next = n_node
-        n_node.next = nxt
         
+        # Preserve the reference to the next node after a_node
+        nxt = a_node.next
+        
+        # Swap the positions of n_node and a_node
+        a_node.next = n_node  # a_node now points to the current node
+        n_node.next = nxt     # n_node now points to the node after a_node
+        
+        # Update the next node after curr to point to a_node
         curr.next = a_node
-
+        
+        # Move curr two steps forward to continue with the next pair of nodes
         curr = curr.next.next
-
-
+    
+    # Return the next node after the dummy node, which represents the modified linked list
     return dummy.next
