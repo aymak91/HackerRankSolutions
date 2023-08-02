6ab13d5c99264571a0acc1960b5b7dff38a0d3c5
Josiah Leon <sjosiahleon@gmail.com>
2023-08-02 11:48:31 -0700

    added comments to my code

--- 2023-07-31/chess_tournament.py
+++ 2023-07-31/chess_tournament.py
@@ -1,15 +1,30 @@
 def getPotentialOfWinner(potential, k):
+    # Initialize a variable to keep track of the number of consecutive rounds won by the current winner
     rounds_won = 0
+
+    # Set the initial winner as the first player in the 'potential' list
     curr_winner = potential[0]
 
+    # Iterate over the 'potential' list starting from the second player (index 1)
     for i in range(1, len(potential)):
+        # Get the potential of the current player
         player = potential[i]
+
+        # Check if the current player's potential is greater than the current winner's potential
         if player > curr_winner:
+            # If the current player has greater potential, reset the count of consecutive rounds won
             rounds_won = 0
+
+            # Update the current winner to the current player
             curr_winner = player
-        
+
+        # Increment the count of consecutive rounds won by the current winner
         rounds_won += 1
+
+        # Check if the current winner has won 'k' consecutive rounds
         if rounds_won == k:
+            # If the current winner has won 'k' consecutive rounds, stop the loop and return the current winner's potential
             break
 
+    # Return the potential of the winner who won 'k' consecutive rounds
     return curr_winner
--- 2023-07-31/mult_req_at_a_time.py
+++ 2023-07-31/mult_req_at_a_time.py
@@ -1,17 +1,27 @@
 import re
 from collections import defaultdict as ddict
 
+# Get the filename as input from the user
 filename = input()
+
+# Create a defaultdict to store timestamps as keys and their occurrence counts as values
 timestamps = ddict(int)
 
+# Read the input file and populate the 'timestamps' dictionary
 with open(filename, 'r') as f:
+    # Read each line in the file
     for line in f.readlines():
+        # Use regular expression to search for timestamps in the format [YYYY/MM/DD:HH:mm:ss]
         match = re.search(r'\[([\d\/\w:]*)', line)
         if match:
+            # If a timestamp is found in the line, increment its occurrence count in the 'timestamps' dictionary
             timestamps[match.group(1)] += 1
-    
+
+# Create a new file with 'req_' prefix and append the timestamps with occurrence count greater than 1
 with open('req_'+filename, 'w') as f:
     for k, v in timestamps.items():
+        # Check if the occurrence count (v) of the timestamp is greater than 1
         if v > 1:
+            # If the timestamp occurred more than once, write it to the new file followed by a new line
             f.write(k)
             f.write('\n')
